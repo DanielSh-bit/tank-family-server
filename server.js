ace6a39c194d2f13e70ed648d06d79cab998c4a2
DanielSh-bit <daniel.shachak@gmail.com>
2025-11-19 19:37:55 +0200

    add node server

--- server.js
+++ server.js
@@ -1,61 +1,32 @@
+const http = require("http");
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 8765 });
+const PORT = process.env.PORT || 10000;
+
+// שרת HTTP בסיסי כדי ש-Render לא יקרוס
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Tank Family WebSocket server is running\n");
+});
+
+// שרת WebSocket אמיתי
+const wss = new WebSocket.Server({ server: server });
 
 let players = {};
-let nextId = 1;
-
-function broadcastState() {
-  const msg = JSON.stringify({
-    type: "state",
-    players: players
-  });
-
-  for (const p of Object.values(players)) {
-    if (p.ws.readyState === WebSocket.OPEN) {
-      p.ws.send(msg);
-    }
-  }
-}
-
-setInterval(() => broadcastState(), 50);
 
 wss.on("connection", (ws) => {
-  const id = nextId++;
-  players[id] = {
-    id,
-    name: "Player" + id,
-    x: Math.random() * 700 + 50,
-    y: Math.random() * 500 + 50,
-    angle: 0,
-    color: "#" + Math.floor(Math.random() * 16777215).toString(16),
-    alive: true,
-    ws
-  };
-
-  ws.send(JSON.stringify({ type: "joined", id }));
+  console.log("Client connected");
 
   ws.on("message", (msg) => {
-    try {
-      const data = JSON.parse(msg);
-
-      if (data.type === "join") {
-        players[id].name = data.name;
-      }
-
-      if (data.type === "input") {
-        players[id].x += data.dir.x * 4;
-        players[id].y += data.dir.y * 4;
-        players[id].angle = Math.atan2(data.dir.y, data.dir.x);
-      }
-    } catch (err) {
-      console.log("MSG error:", err);
-    }
+    console.log("MSG:", msg.toString());
   });
 
   ws.on("close", () => {
-    delete players[id];
+    console.log("Client disconnected");
   });
 });
 
-console.log("Server running...");
+// הפעלה
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
